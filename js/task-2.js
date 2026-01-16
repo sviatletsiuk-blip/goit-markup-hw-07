906bd924bf97335ea8611356251a294adc1db740
sviatletsiuk-blip <sviatletsiuk@gmail.com>
2026-01-16 18:07:18 +0200

    Новий коментар до змін

--- js/task-2.js
+++ js/task-2.js
@@ -29,7 +29,7 @@ const gallery = document.querySelector(".gallery")
 
 const galleryMarkup = images.map(({ url, alt }) =>
   `<li class="gallery-item">
-      <img src="${url}" alt="${alt}"
+      <img src="${url}" alt="${alt}">
   </li>`
 ).join("");
 
