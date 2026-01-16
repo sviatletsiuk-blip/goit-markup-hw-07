cb2beae3f21ad25e4bd1bbf271f3c1a9d5f093cc
sviatletsiuk-blip <sviatletsiuk@gmail.com>
2026-01-16 13:29:35 +0200

    Новий коментар до змін

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+const categoriesList = document.querySelector("#categories")
+const categoriesItems = document.querySelectorAll(".item")
+
+console.log(`Number of categories ${categoriesItems.length}`);
+
+categoriesItems.forEach(item => {
+    const title = item.querySelector("h2").textContent;
+    const elementsCount = item.querySelectorAll("li").length;
+
+    console.log(`Category: ${title}`);
+    console.log(`Elements: ${elementsCount}`);
+});
